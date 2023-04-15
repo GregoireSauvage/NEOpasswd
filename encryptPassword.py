0d7ab288e380419a3d3b7e4b7fc8b1823701799b
GregoireSauvage <78805529+GregoireSauvage@users.noreply.github.com>
2023-04-15 17:41:18 +0200

    fix: bugs fixed and user inputs better handled

--- encryptPassword.py
+++ encryptPassword.py
@@ -1,6 +1,8 @@
 import os
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+from fileManaging import load_data, save_password_data_to_file
+from utilities import enter_credentials, check_master_key, get_key
 
 # Function to encrypt a password
 def encrypt_password(key, password):
@@ -21,3 +23,34 @@ def encrypt_password(key, password):
 
     # Return the IV and ciphertext as bytes
     return iv, ciphertext
+
+# Function to add a password (called from main.py)
+def add_password():
+        
+    try:
+        # Get username, hashed master key, salt and id_service from the json file
+        username, hashed_key, salt, id_service, services = load_data("passwordManager.json")
+
+        # Get username and passphrase from user
+        master_key =  check_master_key(hashed_key, salt)
+
+        # Once the master key is correct, get the email and password to encrypt
+        service, email, password = enter_credentials(services)
+
+        # Encrypt the password
+        iv, cyphertext = encrypt_password(master_key, password)
+        encrypted_password = iv + cyphertext
+
+        print("------------------ ENCRYPTED PASSWORD ------------------")
+        print(encrypted_password)
+
+        # Save the encrypted password to the JSON file
+        json_file = "passwordManager.json"
+        save_password_data_to_file(username, json_file, id_service, service, email, encrypted_password)
+
+    except ValueError as ve:
+        print(ve)
+        exit(0)
+    except KeyboardInterrupt as ke:
+        print("\nExiting...")
+        exit(0)
